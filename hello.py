da499c1b17427682c4651d6955c833dd234404d6
teratsyk <trytheuof@gmail.com>
2017-08-24 16:42:46 +0900

    hello tensorflow

--- hello.py
+++ hello.py
@@ -1,9 +1,20 @@
+import tensorflow as tf
+import multiprocessing as mp
+
 from flask import Flask
-app = Flask(__name__)
+
+app = Flask(__name__) 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    core_num = mp.cpu_count()
+    config = tf.ConfigProto(
+        inter_op_parallelism_threads=core_num,
+        intra_op_parallelism_threads=core_num )
+    sess = tf.Session(config=config)
+
+    hello = tf.constant('hello, tensorflow!')
+    return sess.run(hello)
 
 if __name__ == '__main__':
     app.run()
