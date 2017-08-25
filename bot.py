127a59f77747668f6183e49d0a4a1adb4787e287
teratsyk <trytheuof@gmail.com>
2017-08-25 15:25:52 +0900

    add docomoAPI

--- bot.py
+++ bot.py
@@ -7,14 +7,59 @@ import multiprocessing as mp
 
 from flask import Flask
 from flask import request
+from flask_api import status
 
 LINE_API_REPLY ='https://api.line.me/v2/bot/message/reply'
 LINE_HEADERS = {
     'Content-type': 'application/json',
     'Authorization': 'Bearer {}'.format(os.environ['CHANNEL_ACCESS_TOKEN'])
 }
+DOCOMO_API_DIALOGUE = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue'
+DOCOMO_HEADERS = {
+    'Content-type': 'application/json'
+}
+
+def get_username():
+    '''LINEユーザ名を取得'''
+    return 'お前'
+
+def get_context():
+    '''ユーザごとのコンテキストをredis等から取得'''
+    return ''
+
+def set_context(context):
+    '''ユーザごとのコンテキストをredis等に保存'''
+
+
+def get_dialogue(text):
+    '''入力されたテキストに対するレスポンスを生成する'''
+    response_text = ''
+
+    params = {
+        "utt": text,
+        "context": get_context(),
+        "nickname": get_username(),
+        "mode": "dialog",
+        "t": "20"
+    }
+    r = requests.post(
+        "{}?APIKEY={}".format(DOCOMO_API_DIALOGUE, os.environ['DOCOMO_API_KEY']),
+        data=json.dumps(params),
+        headers=DOCOMO_HEADERS
+    )
+    logging.debug(r)
+    if r.status_code == status.HTTP_200_OK:
+        set_context(r.text.context)
+        response_text = r.text.utt
+
+    return response_text
 
 def send_reply(body):
+    '''
+    リプライ返信を行う
+    テキストが送られたら対話テキストを
+    それ以外は顔文字を返す
+    '''
     for event in body['events']:
         logging.debug(event)
         responses = []
@@ -24,7 +69,7 @@ def send_reply(body):
             text = ''
 
             if message['type'] == 'text':
-                text = message['text']
+                text = get_dialogue(message['text'])
             else:
                 text = '(´・ω・`)'
 
@@ -38,6 +83,7 @@ def send_reply(body):
         requests.post(LINE_API_REPLY, data=json.dumps(reply), headers=LINE_HEADERS)
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/webhook", methods=['POST'])
 def webhook():
@@ -56,4 +102,5 @@ def hello_world():
     return sess.run(hello)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port)
